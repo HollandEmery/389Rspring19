452f16bb2a9047c9b175c8f222e7ee63e166e2e0
HollandEmery <HollandEmery2@gmail.com>
2019-04-26 16:09:26 -0400

    finished HW 9

--- assignments/9_Crypto_I/writeup/README.md
+++ assignments/9_Crypto_I/writeup/README.md
@@ -1,17 +1,17 @@
 # Crypto I Writeup
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Holland Emery
+Section: 0201
 
 I pledge on my honor that I have not given or received any unauthorized
 assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Holland Emery
 
 ## Assignment Writeup
 
 ### Part 1 (70 Pts)
-
+Every thing is in crack.py
 ### Part 2 (30 Pts)
-
+the flag is CMSC389R-{h@sh1ng_@nd_sl@sh1ng}, the code is in server_crack.py
 *Your reflection goes here*
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,15 +6,29 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    #hashes = # open and read hashes.txt
+    passwords = open("passwords.txt", 'r')# open and read passwords.txt
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
     data = s.recv(1024)
+    data = data.replace(">", "")
+    data = data.replace("can you crack this hash?", "")
+    data = data.strip()
+    print(data)
+    for c in characters:
+        for p in passwords:
+            y = c+p
+            y =  y.strip()
+            h = hashlib.sha256(y).hexdigest()
+            if h == data:
+                s.send(y)
+        passwords = open("passwords.txt", 'r')
+    1024)
+    print(data)
     # parse data
     # crack 3 times
 
